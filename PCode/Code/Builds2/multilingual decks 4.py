5605ee8c281afd49b28bb950ed4f1414cde5539d
ClashJB <83531627+ClashJB@users.noreply.github.com>
2025-11-11 00:09:33 +0100

    Wndows commit

---
+++ PCode/Code/Builds2/multilingual decks 4.py
@@ -0,0 +1,394 @@
+import deepl
+import csv
+import sys
+import re
+import pyfiglet
+import glob
+import pandas
+from datetime import datetime, timedelta
+
+auth_key = "7053ad0d-f006-4202-aeb4-b96400b68c58:fx"
+if not auth_key:
+    raise ValueError("Missing DeepL API key. Please set DEEPL_API_KEY in your environment.")
+deepl_client = deepl.DeepLClient(auth_key)
+
+
+langs = []
+temp_langs = []
+target_langues = {
+    "czech": "CS",
+    "danish": "DA",
+    "german": "DE",
+    "british english": "EN-GB",
+    "english": "EN-US",
+    "spanish": "ES",
+    "latino spanish": "ES-419",
+    "estonian": "ET",
+    "finnish": "FI",
+    "french": "FR",
+    "hungarian": "HU",
+    "indonesian": "ID",
+    "italian": "IT",
+    "lithuanian": "LT",
+    "latvian": "LV",
+    "norwegian Bokmål": "NB",
+    "dutch": "NL",
+    "polish": "PL",
+    "brazilian portuguese": "PT-BR",
+    "portuguese": "PT-PT",
+    "romanian": "RO",
+    "slovak": "SK",
+    "slovenian": "SL",
+    "swedish": "SV",
+    "turkish": "TR",
+    "vietnamese": "VI"
+}
+
+deck = "multideck1.csv"
+
+
+def startup():
+    print(pyfiglet.figlet_format("BABELANGUE", font="chunky"))
+    build = sys.argv[0]
+    print(f"Build: {build}")
+    print("Welcome to BABELANGUE!")
+
+class Flashcard:
+    def __init__(self, row):
+        self.row = row
+        self.box = self.get_box()
+        self.next_review = self.get_next_review()
+        self.last_review = self.get_last_review()
+
+    def get_box(self):
+        try:
+            if self.row["box"]:
+                return int(self.row["box"])
+        except KeyError:
+            return 1
+
+    def get_next_review(self):
+        try:
+            if self.row["next_review"]:
+                return pandas.to_datetime(self.row["next_review"])
+        except KeyError:
+            return datetime.today()
+
+    def get_last_review(self):
+        try:
+            if self.row["last_review"]:
+                return pandas.to_datetime(self.row["last_review"])
+        except KeyError:
+            return datetime(1767, 1, 1)
+        
+    def save_row(self):
+        self.row["box"] = self.box
+        self.row["next_review"] = self.next_review
+        self.row["last_review"] = self.last_review
+        return self.row
+
+class Deck:
+    def __init__(self, csv_file, langs=None):
+        self.cards = self.load_from_csv(csv_file)
+        self.csv_file = csv_file
+        if langs:
+            self.langs = langs
+        else:
+            self.langs = self.get_langs()
+        self.fieldnames = ["next_review", "last_review", "box"] + self.langs
+        self.n_cards, self.n_due = self.learn_information()
+
+    def get_langs(self):
+            with open(self.csv_file , mode="r", newline="") as file:
+                reader = csv.DictReader(file)
+                langs = []
+                for name in reader.fieldnames:
+                    if name in target_langues.values():
+                        langs.append(name)
+                return langs
+            
+    def learn_information(self):
+        n_cards = 0
+        n_due = 0
+        for card in self.cards:
+            n_cards += 1
+            if datetime.today() >= card.next_review:
+                n_due += 1
+        return n_cards, n_due
+    
+    def train(self, from_langs, to_langs):
+        with open (self.csv_file, "w", newline="", encoding="utf-8") as deck:
+            writer = csv.DictWriter(deck, self.fieldnames)
+            writer.writeheader()
+        exit_mode = False
+        for card in self.cards:
+            if not datetime.today() >= card.next_review or exit_mode:
+                with open (self.csv_file, "a", newline="", encoding="utf-8") as deck:
+                    writer = csv.DictWriter(deck, self.fieldnames)
+                    writer.writerow(card.save_row())
+            else:
+                q = []
+                a = []
+                for lang in from_langs:
+                    q.append(f"{lang}: {card.row[lang]}")
+                for lang in to_langs:
+                    a.append(f"{lang}: {card.row[lang]}")
+                
+                try:
+                    print(f"Front Side: {"   |   ".join(q)}")
+                    msvcrt.getch()
+                    print(f" Back Side: {"   |   ".join(a)}")
+                    guess = msvcrt.getch()
+                    if guess == b"x":
+                        raise EOFError
+            
+                    if guess == b" ":
+                        print("Good job!")
+                        card.box += 1
+                    else:
+                        print("Don't worry you'll get it next time.")
+                        card.box = 1
+                except EOFError:
+                    exit_mode = True
+                    print("User exists program, but files are saved.")
+                # schedule next review
+                interval_days = {1: 1, 2: 3, 3: 7, 4: 14}.get(card.box, 30)
+                card.next_review = datetime.today() + timedelta(days=interval_days)
+                
+                with open (self.csv_file, "a", newline="", encoding="utf-8") as deck:
+                    writer = csv.DictWriter(deck, self.fieldnames)
+                    writer.writerow(card.save_row())
+
+
+def add_langs():  #maybe Deck Class
+    print("Please enter all the languages.")
+    langs = []
+    while True:
+        
+        try:
+            l_input = input("Add a language: ").strip().lower()
+            if not l_input and 1 < len(langs):
+                break
+            if not target_langues[l_input] in langs:
+                langs.append(target_langues[l_input])
+        except KeyError:
+            print("Supported languages are:")
+            numbered_langues = {i: lang for i, lang in enumerate(target_langues.keys(), start=1)}
+            print(numbered_langues)
+            for number, word in numbered_langues.items():
+                print(f"{number} | {word}")
+            try:
+                input_number = int(input("Choose the lang as number: ").strip())
+                n_input = numbered_langues[input_number]
+                if not target_langues[n_input] in langs:
+                    langs.append(target_langues[n_input])
+            except ValueError:
+                print("Not a number or not in list")
+    return langs
+
+def fix_langs(source_lang):  #maybe deck Class
+    if source_lang == "EN":
+        return "EN-US"
+    elif source_lang == "PT":
+        return "PT-PT"
+    else:
+        return source_lang
+
+def decks_info(): #redone & classless
+    deck_files = glob.glob("*.csv")
+    decks_info = []
+    for f in deck_files:
+        deck = Deck(f)
+        langs = deck.langs
+        row_count = len(deck.cards)
+        name = deck.csv_file
+        
+        decks_info.append((name, row_count, langs))
+    return decks_info
+
+def print_decklist(decks_info): #Could be put into Deck, but unnecessary
+    print("\nDecks found:")
+    for i, (file, n_row, langs) in enumerate(decks_info, 1):
+        print(f"[{i}] {file}  ({n_row} cards) | {", ".join(langs[:-1])} and {langs[-1]}")
+
+def translate(input, langs):
+    row = {}
+    for lang in langs:
+        result = deepl_client.translate_text(input, target_lang= lang)
+        row[lang] = result
+        source_lang = fix_langs(result.detected_source_lang)
+    if not source_lang in langs:
+        print("The detected language does not match your deck languages, but we tried our best translating them quand-meme.")
+    return row, source_lang
+
+def new_deck_mode():
+    deck = input("How would you like to name your new deck?\nName: ").strip() + ".csv"
+    langs = add_langs()
+    return deck, langs
+
+
+
+
+def add_cards_mode(deck, langs, new_deck= False):  #put in deck Class
+    if new_deck == True:
+        filemode = "w"
+    elif new_deck == False:
+        filemode = "a"
+    with open(deck, filemode, newline="", encoding="utf-8") as deck:
+        writer = csv.DictWriter(deck, fieldnames= langs)
+        if new_deck:
+            writer.writeheader()
+        try:
+            while True:
+                user_input = input(">")
+                if not user_input:
+                    break
+
+                row, source_lang = translate(user_input, langs)
+
+                while True:
+                    for lang, result in row.items():
+                        if lang == source_lang:
+                            print(f"{source_lang} | {result}")
+                        else:
+                            pass
+                    for lang, result in row.items():
+                            if lang == source_lang:
+                                pass
+                            else:
+                                print(f"--{lang}--> {result}")
+                    
+                    edit_lang = input("Edit: ")
+                    if not edit_lang:
+                        break
+                    elif edit_lang in langs:
+                        row[edit_lang] = input(f"Edit card side (current: {row[edit_lang]}): ")
+                writer.writerow(row)
+        except EOFError:
+            sys.exit()
+
+def print_cards(file): #for deck Class
+    with open(file, mode="r", newline="") as deck:
+        reader = csv.DictReader(deck)
+        for i, row in enumerate(reader):
+            print(f"Card {i + 1}:")
+            for lang, result in row.items():
+                print(f"    {lang} | {result}")
+
+def existing_edit_deck_mode(deck): #I guess it's for Deck Class
+    pd_deck = pandas.read_csv(deck)
+    while True:
+        card_number = input("\nTo edit, enter card number: ")
+        if not card_number:
+            break
+        card_number = int(card_number) - 1
+        print(pd_deck.loc[[card_number]])
+        while True:
+            pd_lang = input("\nEnter the language of the card side you wish to edit: ").upper()
+            if not pd_lang:
+                break
+            if pd_lang in pd_deck.loc[0]:
+                pd_deck.loc[card_number, pd_lang] = input(f"Edit card side (current: {pd_deck.loc[card_number, pd_lang]}): ")
+                pd_deck.to_csv(deck, index=False)
+
+def main():
+    startup()
+    while True:
+        deck_mode = input("\n[N]ew deck | [L]oad deck | [Q]uit Program\nInput: ").strip().lower()
+
+# Existing Deck mode:
+        if deck_mode == "l": 
+            decks_info = decks_info()
+            print_decklist(decks_info)
+            while True:
+                try:
+                    choice = int(input("\nWhich file do you want to open?\nEnter a number: "))
+                    deck = decks_info[choice - 1][0]
+                    print(deck)
+                    break
+                except (ValueError):
+                    print("Please enter a number.")
+                except IndexError:
+                    print(f"Number is out of range. Choose number from 1 to {len(decks_info)}")
+
+            while True:
+                mode = input("\nEnter your mode. [A]dd cards | [S]how cards | [T]rain deck | [Q]uit deck\nMode: ").strip().lower()
+                if mode == "a":
+                    langs = get_langs(deck)
+                    add_cards_mode(deck, langs)
+                elif mode == "q":
+                    break
+                elif mode == "s":
+                    print(deck)
+                    print_cards(deck)
+                    existing_edit_deck_mode(deck)
+                elif mode == "t":
+                    train_deck = Deck(deck)
+                    print("Available languages:")
+                    langs = train_deck.get_langs()
+                    for lang in langs:
+                        print(lang)
+
+                    from_langs = []
+                    to_langs = []
+
+                    while True:
+                        if (len(langs) - len(from_langs)) == 1:
+                            break
+                        from_lang = input("from which language(s) do you want to learn? (end with Enter) ").upper()
+                        if not from_lang and len(from_langs) >= 1:
+                            break
+                        elif from_lang in langs:
+                            from_langs.append(from_lang)
+                        else:
+                            print("Please try again.")
+
+                    while True:
+                        if (len(langs) - len(from_langs) - len(to_langs)) == 1:
+                            for lang in langs:
+                                if not lang in from_langs and not lang in to_langs:
+                                    to_langs.append(lang)
+                                    stop = True
+                                    break
+                            if stop:
+                                break
+                        to_lang = input("What will be your answer language? (end with Enter) ").upper()
+                        if not to_lang and len(to_langs) >= 1:
+                            break
+                        elif to_lang in langs and not to_lang in from_langs:
+                            to_langs.append(to_lang)
+                        else:
+                            print("Please try again.")
+
+                    print(f"Question language(s): {", ".join(from_langs)}")
+                    print(f"Answer language(s): {", ".join(to_langs)}")
+
+                    train_deck.train(from_langs= from_langs, to_langs= to_langs)
+
+
+
+# New Deck mode:
+        elif deck_mode == "n": 
+            deck, langs = new_deck_mode()
+            add_cards_mode(deck, langs, new_deck= True)
+        else:
+            sys.exit()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Edit existing cards??? CHECK
+    # In [S]how cards: 'To edit card, enter its number: KINDA CHECK
+    # When translating: 'Enter result lang to edit result: '  CHECK
+# add deck langs to load deck output???  CHECK
+# Non-translate mode
+# Train mode
+    # feedback system (maybe from 0-1)
+    # False responses are asked again at the end
+    
+# EOFError and in general the exception policy of the program.
+# empty .csv files break the list, because they don't have fieldnames
+# Error message when decks don't have cards or enter edit mode directly
+# Tabulate and pandas integration
