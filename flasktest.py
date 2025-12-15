d9b61bf7ac12ebf9c66e2eee3e5ed35dbaa00a09
ClashJB <julian@biehl.de>
2025-12-15 15:37:34 -0500

    Experimenting with desktop apps

--- flasktest.py
+++ flasktest.py
@@ -525,4 +525,6 @@ def deck_edit(deck):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    import sys
+    debug_mode = '--debug' in sys.argv
+    app.run(host="127.0.0.1", port=5000, debug=debug_mode)
